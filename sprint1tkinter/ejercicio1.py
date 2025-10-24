6f24b77f6ac9b86b4501963201a437b1b8138927
AdrianRocaS <arocas24@fpcoruna.afundacion.org>
2025-10-24 11:02:37 +0200

    Ejercicio 1: Label

--- sprint1tkinter/ejercicio1.py
+++ sprint1tkinter/ejercicio1.py
@@ -1,1 +1,28 @@
+import tkinter as tk
 
+def cambiar_texto():
+    label03.config(text="Nuevo texto")
+
+root = tk.Tk()
+root.title("Ejercicio 1: Label")
+root.geometry("300x150")
+
+label01 = tk.Label(root, text="Bienvenido a Tkinter")
+label01.pack(pady=5)
+
+label02 = tk.Label(root, text="Adrián Roca Santiago")
+label02.pack(pady=5)
+
+label03 = tk.Label(root, text="Texto original")
+label03.pack(pady=5)
+
+
+button01 = tk.Button(root, text="Cambiar texto", command=cambiar_texto)
+button01.pack(pady=10)
+
+label01.pack()
+label02.pack()
+label03.pack()
+button01.pack()
+
+root.mainloop()
