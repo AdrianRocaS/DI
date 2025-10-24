696ea6515bb2f61c06380e568e8503c91ebfc01d
AdrianRocaS <arocas24@fpcoruna.afundacion.org>
2025-10-24 18:03:38 +0200

    Ejercicio 5: Radiobutton

---
+++ sprint1tkinter/ejercicio5.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+from tkinter import Radiobutton
+
+
+#Cambia el color de fondo dependiendo del color seleccionado
+def cambiar_color():
+    root.config(bg=var.get())
+
+#Creación de la ventana
+root = tk.Tk()
+root.title("Ejercicio 5: Radiobutton")
+root.geometry("300x150")
+
+#Variable del color
+var = tk.StringVar()
+
+#Botones
+tk.Radiobutton(root, text="Rojo", variable=var, value="red", command=cambiar_color).pack()
+
+tk.Radiobutton(root, text="Verde", variable=var, value="green", command=cambiar_color).pack()
+
+tk.Radiobutton(root, text="Azul", variable=var, value="blue", command=cambiar_color).pack()
+
+
+#Ejecutar el bucle principal
+root.mainloop()
