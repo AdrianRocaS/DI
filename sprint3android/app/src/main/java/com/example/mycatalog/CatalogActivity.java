577f5c18fc479dbe86c253659458ef5788ba6f17
AdrianRocaS <arocas24@fpcoruna.afundacion.org>
2025-11-05 16:52:38 +0100

    Ejercicio 02

--- sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -1,6 +1,9 @@
 package com.example.mycatalog;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,5 +14,15 @@ public class CatalogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+
+        Button boton02 = findViewById(R.id.button);
+        boton02.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(CatalogActivity.this, DetailActivity.class);
+            startActivity(intent);
+            }
+        });
     }
 }
